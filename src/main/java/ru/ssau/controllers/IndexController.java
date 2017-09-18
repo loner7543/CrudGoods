971a95488a69e2bccac3c944e8cead7d37a98815
loner7543 <Sachok1394@mail.ru>
2017-09-18 20:48:45 +0400

    add missing dependencies, add ProducrService? get all products work

--- src/main/java/ru/ssau/controllers/IndexController.java
+++ src/main/java/ru/ssau/controllers/IndexController.java
@@ -1,20 +1,24 @@
 package ru.ssau.controllers;
 
+import model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import service.ProductService;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
     @Autowired
-    private DriverManagerDataSource dataSource;
+    private ProductService productService;
+
+
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String slash(Model model, HttpSession session) {
         return "redirect:/welcome";
@@ -29,6 +33,7 @@ public class IndexController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/test")
     public void testDb(){
-
+        List<Product> products = productService.getAll();
+        String s = "derfrf";
     }
 }
